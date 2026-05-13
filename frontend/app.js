3dcf73c401c68d71b861eb7a2152a3c27c907888
Daiana Andereggen <daianaandereggen463@gmail.com>
2026-05-13 12:21:26 +0200

    pixel meta

--- frontend/app.js
+++ frontend/app.js
@@ -22,6 +22,7 @@ const state = {
 
 document.addEventListener('DOMContentLoaded', () => {
   initCookieBanner();
+  initMetaPixel();
   initMobileNav();
   initDatePicker();
   initStepNavigation();
@@ -653,6 +654,29 @@ function grantAnalytics(granted) {
       });
     }
   } catch(e) {}
+  if (granted) loadMetaPixel();
+}
+
+function loadMetaPixel() {
+  if (window._nocteaPixelLoaded) return;
+  window._nocteaPixelLoaded = true;
+  (function(f,b,e,v,n,t,s){
+    if(f.fbq)return;
+    n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s);
+  })(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init','1669616460740520');
+  fbq('track','PageView');
+}
+
+function initMetaPixel() {
+  var consent = getCookieConsent();
+  if (consent === 'accepted' || consent === 'custom-accepted') {
+    loadMetaPixel();
+  }
 }
 
 function footerResetCookies() {
